9a0f3501f5792d288624134cfe5583673bb265b2
kr0101 <mohamed442010177@gmail.com>
2022-02-05 10:32:08 +0300

    setup videos bar

---
+++ pages/script.js
@@ -0,0 +1,47 @@
+const vid = document.getElementById("vid")
+
+
+function bnBaz(){
+
+          vid.innerHTML=
+           `
+                <iframe id="wow" style="border-radius: 10px;" width="250" height="261" class="embed-responsive-item"
+                    src="https://www.youtube.com/embed/VEbpULjl9a8" title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+    
+                </iframe>
+    
+          `
+}
+
+function fozan(){
+   
+  vid.innerHTML=
+    `
+   
+         <iframe id="wow"style="border-radius: 10px;" width="250" height="261" class="embed-responsive-item"
+             src="https://www.youtube.com/embed/i09RyC1zOJ4" title="YouTube video player" frameborder="0"
+             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+             allowfullscreen>
+
+         </iframe>
+
+
+     `
+
+  
+}
+
+function alKames(){
+   
+    vid.innerHTML=
+                `
+                <iframe id="wow" style="border-radius: 10px;" width="250" height="261" class="embed-responsive-item"
+                    src="https://www.youtube.com/embed/bnvzfQjQHBw" title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+
+                </iframe>
+`
+}
