5165a97461addbf997a32801bfe5693ec48e1bb9
kr0101 <mohamed442010177@gmail.com>
2022-02-06 07:33:30 +0300

    page #3

--- pages/script.js
+++ pages/script.js
@@ -1,5 +1,7 @@
 const vid = document.getElementById("vid")
 
+ // #page 1
+
 
 function bnBaz(){
 
@@ -43,5 +45,85 @@ function alKames(){
                     allowfullscreen>
 
                 </iframe>
-`
+        `
 }
+
+
+    // #page 2 
+
+
+function saad(){
+
+    vid.innerHTML=
+
+    `<iframe style="border-radius: 10px;" width="250" height="261" class="embed-responsive-item" src="https://www.youtube.com/embed/PZR0iwCdbT8"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>`
+}
+
+function nbel(){
+
+    vid.innerHTML=
+
+    `<iframe style="border-radius: 10px;" width="250" height="261" class="embed-responsive-item" src="https://www.youtube.com/embed/vuoadI9bo3o"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>`
+}
+
+function alKames2(){
+
+    vid.innerHTML=
+
+    `<iframe style="border-radius: 10px;" width="250" height="261" class="embed-responsive-item" src="https://www.youtube.com/embed/NbjDV3sC3N4"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>`
+}
+
+
+// #page 3
+
+function ahmed(){
+
+    vid.innerHTML=
+
+   ` <iframe style="border-radius: 10px;" width="250" height="261" class="embed-responsive-item" src="https://www.youtube.com/embed/EMf0LhEbO6Y"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+        
+                </iframe>`
+        
+}
+
+function otemin(){
+
+    vid.innerHTML=
+
+   ` <iframe style="border-radius: 10px;" width="250" height="261" class="embed-responsive-item" src="https://www.youtube.com/embed/WrCI47-iguU"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+        
+                </iframe>`
+        
+}
+
+function alKames3(){
+
+    vid.innerHTML=
+
+    ` <iframe style="border-radius: 10px;" width="250" height="261" class="embed-responsive-item" src="https://www.youtube.com/embed/nXdCniOqp2I"
+                     title="YouTube video player" frameborder="0"
+                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                     allowfullscreen>
+         
+                 </iframe>`
+}
+
+
+//#page 4
+
+
